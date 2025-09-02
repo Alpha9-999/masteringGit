d45744c7fe6537cb62501f6fccbea75d4591214d
George Opare <medassist1001@gmail.com>
2025-09-02 14:59:39 +0000

    third commit

---
+++ b.py
@@ -0,0 +1,10 @@
+# print('hello')
+# git init
+# git add hello.py
+# git commit -m 'first commit'
+# git status
+# git checkout
+# git branch -M main
+# git push -u origin main
+# git remote add origin
+# git checkout -b newBranch
