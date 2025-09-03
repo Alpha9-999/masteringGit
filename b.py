876e95cba35ef48be805d227c0be601ddd6064c2
George Opare <medassist1001@gmail.com>
2025-09-03 09:09:07 +0000

    6th commit

--- b.py
+++ b.py
@@ -8,9 +8,20 @@
 # git push -u origin main
 # git remote add origin
 # git checkout -b newBranch
-age = int(input('What is your age? '))
-if age >= 18:
-    print('You are welcome')
+# age = int(input('What is your age? '))
+# if age >= 18:
+#     print('You are welcome')
+# else:
+#     print('get out')
+# print('I\'m learning git')
+# age = 19
+# message = 'welcome' if age > 18 else 'get away'
+# print(message)
+# and
+# or
+# not
+age = int(input('What is your age?'))
+if 18 <= age < 65:
+    print('Welcome')
 else:
-    print('get out')
-print('I\'m learning git')
+    print('get away')
